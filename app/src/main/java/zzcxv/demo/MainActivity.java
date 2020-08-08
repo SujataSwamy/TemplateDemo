b4ecef140abeb8f61c5253488fe51ebb7febe3e9
Sujata Swamy <sujata.swamy@airfi.aero>
2020-08-09 02:29:35 +0530

    sljdfl

--- app/src/main/java/zzcxv/demo/MainActivity.java
+++ app/src/main/java/zzcxv/demo/MainActivity.java
@@ -1,7 +1,8 @@
-package com.app.demonow;
+package zzcxv.demo;
 
 import android.os.Bundle;
 
+import com.app.demonow.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
