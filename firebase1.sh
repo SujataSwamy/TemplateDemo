e15eee42d34a8a726ddde1ef055433b0edbeef18
Sujata Swamy <sujata.swamy@airfi.aero>
2020-08-09 12:17:42 +0530

    firebase

--- firebase1.sh
+++ firebase1.sh
@@ -27,6 +27,7 @@ gcloud firebase test android versions list
 gcloud firebase test android run --app app/build/outputs/apk/deb ug/app-debug.apk --type=robo --device model=Pixel2,version=28
 gcloud firebase test android run \
     --type instrumentation \
+    --app app/build/outputs/apk/debug/app-debug.apk \
     --test app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk \
     --device model=Pixel2,version=28,locale=en,orientation=portrait  \
     --device model=NexusLowRes,version=24,locale=en,orientation=portrait
